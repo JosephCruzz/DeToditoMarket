dad82185addc9535524d5c50d2067e3cfbffa78b
Joseph Cruz <josephcruz05@outlook.com>
2025-11-23 14:28:31 -0600

    Crear Compra

--- controllers/compras.js
+++ controllers/compras.js
@@ -1,25 +1,32 @@
-const compras = require ("../models/compras");
-const detalleCompras = require ("../models/detalleCompra");
+const { compras, sequelize } = require("../models");
+const detalleCompras = require("../models/detalleCompra");
 
 exports.addCompra = async (req, res) => {
-    try {
-        const {id, proveedor_id, user_id, estado } = req.body;
+  try {
+    const { proveedor_id, user_id, estado } = req.body;
 
-        const newCompra = await compras.create({
-            id,
-            proveedor_id,
-            user_id,
-            estado
-        });
-
-        res.status(201).json(newCompra);
-
-    } catch (err) {
-        console.error("Hubo un error al añadir la compra:", err);
-
-        res.status(500).json({
-            message: "Hubo un error al crear la compra",
-            error: err.message
-        });
+    if (!user_id) {
+      return res.status(400).json({
+        message: "El campo user_id tiene que tener valores existentes",
+      });
     }
+    const newCompra = await compras.create({
+      proveedor_id,
+      user_id,
+      estado: estado || "pendiente",
+    });
+    res.status(201).json(newCompra);
+  } catch (err) {
+    if (err.name === "SequelizeForeignKeyConstraintError") {
+      return res.status(400).json({
+        message:
+          "El id de foreign key de proveedor_id o user_id no es existente en la base de datos",
+      });
+    }
+
+    res.status(500).json({
+      message: "Hubo un error al crear la compra",
+      error: err.message,
+    });
+  }
 };
