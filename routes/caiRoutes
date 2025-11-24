67d8da4468b85cead0aebae4f256acf22e2114c7
JosephCruzz <josephcruz0503@gmail.com>
2025-11-24 21:58:20 +0000

    1st

---
+++ routes/caiRoutes
@@ -0,0 +1,8 @@
+import { addCai } from "../controllers/cai";
+
+const express = require("express");
+const Routes = express.Router();
+
+Routes.post("/crear",addCai);
+
+module.exports = Routes;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const models = require("./models");
 const sequelize = require("./config/database");
 const comprasRoutes = require("./routes/comprasRoutes");
 const detalleCompraRoutes = require("./routes/detalleCompraRoutes");
+const caiRoutes = require("./routes/caiRoutes.js");
 
 // Middleware
 app.use(express.json());
@@ -37,5 +38,6 @@ pide algo del front end
 
 app.use("/compra", comprasRoutes);
 app.use("/detalleCompra", detalleCompraRoutes);
+app.use("/cai", caiRoutes);
 
 app.get("/ping", (req, res) => res.send("pong"));
