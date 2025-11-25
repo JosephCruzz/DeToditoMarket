bee29a5f5dae839dca02496d8357dee4badffa7e
Joseph Cruz <josephcruz05@outlook.com>
2025-11-24 18:20:26 -0600

    Avance con Cai

--- controllers/cai.js
+++ controllers/cai.js
@@ -1,30 +1,60 @@
-const {cai} = require("../models");
+const { cai } = require("../models");
 
-exports.addCai = async (req,res) => {
-    const {codigo_cai, rango_inicial, rango_final, fecha_autorizacion, fecha_limite_emision, estado, observaciones} = req.body;
+exports.addCai = async (req, res) => {
+  const {
+    codigo_cai,
+    rango_inicial,
+    rango_final,
+    fecha_autorizacion,
+    fecha_limite_emision,
+    estado,
+    observaciones,
+  } = req.body;
 
+  if (
+    !codigo_cai ||
+    !rango_inicial ||
+    !rango_final ||
+    !fecha_autorizacion ||
+    !fecha_limite_emision
+  ) {
+    return res.status(400).json({
+      message:
+        "Los campos codigo_cai, rango_inicial, rango_final, y campos fechas son obligatorios",
+    });
+  }
 
-    //posibles verificaciones mas a fondo sobre 
-    /*
-    Si codigo rangos son int base de datos estan en varchar verificar eos
-    verificar fecha y fecha que formato de fecha se utilizara en front end para ponerlo a
-     */
-    try {
-    const addC = cai.create({
-        codigo_cai,
-        rango_inicial,
-        rango_final,
-        fecha_autorizacion, 
-        fecha_limite_emision,
-        estado,
-        observaciones
-    })
+  const fechaAth = new Date(fecha_autorizacion);
+  const fechaLimiteEm = new Date(fecha_limite_emision);
+
+  if (isNaN(fechaAth.getTime()) || isNaN(fechaLimiteEm)) {
+    return res.status(400).json({
+      message: "Porfavor ingresar una fecha válida YYYY-MM-DD, y real",
+    });
+  }
+
+  if (fechaAth > fechaLimiteEm) {
+    return res.status(400).json({
+      message:
+        "La fecha de autorización no puede ser mayor que la fecha límite",
+    });
+  }
+
+  try {
+    const addC = await cai.create({
+      codigo_cai,
+      rango_inicial,
+      rango_final,
+      fecha_autorizacion,
+      fecha_limite_emision,
+      estado,
+      observaciones: observaciones || "Ninguna Observación",
+    });
     res.status(201).json(addC);
-    } catch (err) {
-        res.status(500).json({
-            message: "No se pudo crear Cai",
-            Error: err.message
-        })
-    }
-   
-}
+  } catch (err) {
+    res.status(500).json({
+      message: "No se pudo crear Cai",
+      Error: err.message,
+    });
+  }
+};
