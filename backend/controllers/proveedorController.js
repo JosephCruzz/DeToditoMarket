a115edb3f7246031eec9a80d203f7a204cd3ac94
bryanbanegas <140980028+bryanbanegas@users.noreply.github.com>
2025-11-25 11:45:21 -0600

    editUser para editar los atributos de un usuario existente

--- backend/controllers/proveedorController.js
+++ backend/controllers/proveedorController.js
@@ -30,3 +30,31 @@ exports.addSupplier = async (request, response) => {
         response.status(500).json({error: error.message});
     }
 }
+
+exports.editSupplier = async (request, response) => {
+    try{
+        const { id } = request.params;
+        const { 
+            nombre,
+            telefono,
+            direccion,
+            estado
+        } = request.body;
+
+        const supplierEdit = await Proveedor.findByPk(id);
+        if(!supplierEdit){
+            return response.status(404).json({message: "No se encontro el proveedor."});
+        }
+
+        await supplierEdit.update({
+            nombre,
+            telefono,
+            direccion,
+            estado
+        });
+
+        response.json({message: "Proveedor editado con exito."});
+    }catch(error){
+        response.status(500).json({error: error.message});
+    }
+}
