8aa3e5f2b5add699dfc5d0d4eee91394bccdcdad
Joseph Cruz <josephcruz05@outlook.com>
2025-12-02 20:23:34 -0600

    v1.0.0

--- controllers/detalleDeVenta.js
+++ controllers/detalleDeVenta.js
@@ -1,17 +1,16 @@
 const { DetalleVenta } = require("../models");
 
+const arrFields = [
+  { name: "factura_id", type: "number" },
+  { name: "producto_id", type: "number" },
+  { name: "cantidad", type: "number" },
+  { name: "precio_unitario", type: "number" },
+  { name: "descuento", type: "number" },
+  { name: "total_linea", type: "number" },
+  { name: "metodo_pago", type: "string" },
+  { name: "observaciones", type: "string" },
+];
 exports.addDetalleVenta = async (req, res) => {
-  const arrFields = [
-    { name: "factura_id", type: "number" },
-    { name: "producto_id", type: "number" },
-    { name: "cantidad", type: "number" },
-    { name: "precio_unitario", type: "number" },
-    { name: "descuento", type: "number" },
-    { name: "total_linea", type: "number" },
-    { name: "metodo_pago", type: "string" },
-    { name: "observaciones", type: "string" },
-  ];
-
   try {
     for (const fields of arrFields) {
       if (!req.body[fields.name] && req.body[fields.name] !== 0) {
@@ -42,6 +41,30 @@ exports.addDetalleVenta = async (req, res) => {
           "numero de factura y/o numero de numero de producto no es existente.",
       });
     }
+    return res.status(500).json({
+      status: "Error",
+      message: err.message,
+    });
+  }
+};
+
+exports.getDetalleVenta = async (req, res) => {
+  try {
+    const getDetalleV = await DetalleVenta.findAll({
+      order: [["fecha", "ASC"]],
+    });
+
+    if (getDetalleV.length === 0) {
+      return res.status(404).json({
+        status: "Error",
+        message: "No se encontro detalles de ventas.",
+      });
+    }
+    res.status(200).json({
+      status: "Success",
+      message: getDetalleV,
+    });
+  } catch (err) {
     return res.status(500).json({
       status: "Error",
       message: err.message,
