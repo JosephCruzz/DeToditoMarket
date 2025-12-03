d43caec0588986645422ce4c5e5bd6f844d852e3
Andrea Quin <andrea.quin123@gmail.com>
2025-12-02 19:11:44 -0600

    Agregando deleteComprobante junto a su respectivas validaciones.

--- routes/comprobantes.js
+++ routes/comprobantes.js
@@ -6,6 +6,7 @@ const comprobantesController = require("../controllers/comprobantes.js");
 router.post("/crear", comprobantesController.addComprobante);
 router.put("/:id", comprobantesController.editComprobante);
 router.get("/:id", comprobantesController.getComprobante);
+router.delete("/:id", comprobantesController.deleteComprobante);
 
 
 module.exports = router;
