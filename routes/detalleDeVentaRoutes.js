18b1e166e31c0bcee1cec1cf8f5c1b74d55254a5
Joseph Cruz <josephcruz05@outlook.com>
2025-12-04 09:42:04 -0600

    v1.0.0

--- routes/detalleDeVentaRoutes.js
+++ routes/detalleDeVentaRoutes.js
@@ -6,5 +6,6 @@ const detalleDeVentaC = require("../controllers/detalleDeVenta");
 routes.post("/crear", detalleDeVentaC.addDetalleVenta);
 routes.get("/", detalleDeVentaC.getDetalleVenta);
 routes.put("/editar/:id", detalleDeVentaC.editDetalleVenta);
+routes.delete("/eliminar/:id", detalleDeVentaC.deleteDetalleVenta);
 
 module.exports = routes;
