676ee447d3524382b400955ad0890e4f82275ed6
bryanbanegas <140980028+bryanbanegas@users.noreply.github.com>
2025-12-13 14:52:57 -0600

    App.js arreglado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import { Container } from 'react-bootstrap';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from './components/Header.jsx';
-import GestionProductos from './components/GestionProductos.jsx';
-import GestionUsuarios from './components/GestionUsuarios.jsx';
-import GestionProveedores from './components/GestionProveedores.jsx';
-import ReportesVentas from './components/ReportesVentas.jsx';
-import ReportesCompras from './components/ReportesCompras.jsx';
-import Auditoria from './components/Auditoria.jsx';
 
 function App() {
-  
   return (
-    <Router>
-      <Container>
-        <Header/> 
-        <Routes>
-          <Route path="/" element={<GestionProductos/>}/>
-          <Route path="/gestion/productos" element={<GestionProductos/>}/>
-          <Route path="/gestion/usuarios" element={<GestionUsuarios/>}/>
-          <Route path="/gestion/proveedores" element={<GestionProveedores/>}/>
-          <Route path="/reportes/ventas" element={<ReportesVentas/>}/>
-          <Route path="/reportes/compras" element={<ReportesCompras/>}/>
-          <Route path="/reportes/auditoria" element={<Auditoria/>}/>
-        </Routes>
-      </Container>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
