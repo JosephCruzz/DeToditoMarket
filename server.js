4b86b1cb3aa2f564e36f92564030a123e2521e0a
bryanbanegas <140980028+bryanbanegas@users.noreply.github.com>
2025-12-13 21:55:24 -0600

    pruebass

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const cors = require("cors");
 const app = express();
 var logger = require('morgan');
 var path = require('path');
